c67523dac207c4f52683e1726c4d7ae82cf3646a
evgenyk <a@1.com>
2020-10-17 14:53:50 +0300

    fix deploy add test

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ docker push evgenik22/multi-server:$SHA
 docker push evgenik22/multi-worker:$SHA
 
 
-kubectl apply -f k8s
+kubectl apply -f k8s/
 kubectl set image deployments/client-deployment client=evgenik22/multi-client:$SHA
 kubectl set image deployments/server-deployment server=evgenik22/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=evgenik22/multi-worker:$SHA
